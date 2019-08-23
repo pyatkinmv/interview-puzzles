943ce9bb7133bd4ef13fc0d8b1ce75419bd24f56
pyatkinmv <pyatkinmv@gmail.com>
2019-08-23 23:21:42 +0300

    Create three impls of function finding the Kth to last element of linked list: dummy; recursive; iterative

---
+++ src/main/java/ru/pyatkinmv/puzzles/lists/KthToLastElementGetter.java
@@ -0,0 +1,97 @@
+package ru.pyatkinmv.puzzles.lists;
+
+import ru.pyatkinmv.puzzles.lists.DummyLinkedList.Entry;
+
+public class KthToLastElementGetter<E> {
+    private static final String INDEX_EXCEED_LIST_SIZE = "k > list.size";
+
+    public E get(int k, DummyLinkedList<E> list) {
+        int size = getSize(list);
+        if (k > size) {
+            throw new IllegalArgumentException(INDEX_EXCEED_LIST_SIZE);
+        }
+
+        int n = size - k + 1;
+
+        int i = 0;
+        for (Entry<E> entry = list.getFirst(); entry != null; entry = entry.next) {
+            ++i;
+            if (i == n) {
+                return entry.item;
+            }
+        }
+
+        throw new IllegalArgumentException("k < 1");
+    }
+
+    private int getSize(DummyLinkedList list) {
+        int size = 0;
+        for (Entry entry = list.getFirst(); entry != null; entry = entry.next) {
+            ++size;
+        }
+        return size;
+    }
+
+    public E getIterative(int k, DummyLinkedList<E> list) {
+        validateIndex(k);
+        Entry<E> e2 = list.getFirst();
+        int i2 = 0;
+        while (i2 != k) {
+            if (e2 == null) {
+                throw new IllegalArgumentException(INDEX_EXCEED_LIST_SIZE);
+            }
+            ++i2;
+            e2 = e2.next;
+        }
+        Entry<E> e1 = list.getFirst();
+        while (e2 != null) {
+            e1 = e1.next;
+            e2 = e2.next;
+        }
+        return e1.item;
+    }
+
+
+    public E getRecursive(int k, DummyLinkedList<E> list) {
+        validateIndex(k);
+        Entry<E> entry = getRecursive(k, new MutableInt(0), list.getFirst());
+        if (entry == null) {
+            throw new IllegalArgumentException(INDEX_EXCEED_LIST_SIZE);
+        }
+        return entry.item;
+    }
+
+    private Entry<E> getRecursive(int k, MutableInt index, Entry<E> head) {
+        if (head == null) {
+            return null;
+        }
+        Entry<E> entry = getRecursive(k, index, head.next);
+        index.setValue(index.getValue() + 1);
+        if (k == index.getValue()) {
+            return head;
+        }
+        return entry;
+    }
+
+    private void validateIndex(int k) {
+        if (k < 1) {
+            throw new IllegalArgumentException("k < 1");
+        }
+    }
+
+    private class MutableInt {
+        private int value;
+
+        MutableInt(int value) {
+            this.value = value;
+        }
+
+        int getValue() {
+            return value;
+        }
+
+        void setValue(int value) {
+            this.value = value;
+        }
+    }
+}
---
+++ src/test/java/ru/pyatkinmv/puzzles/lists/KthToLastElementGetterTest.java
@@ -0,0 +1,105 @@
+package ru.pyatkinmv.puzzles.lists;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class KthToLastElementGetterTest {
+    private KthToLastElementGetter<String> getter;
+    private DummyLinkedList<String> list;
+    private DummyLinkedList<String> emptyList;
+    private DummyLinkedList<String> listWithOneElem;
+
+    @Before
+    public void setUp() {
+        getter = new KthToLastElementGetter<>();
+
+        list = new DummyLinkedList<>();
+        list.addLast("one");
+        list.addLast("two");
+        list.addLast("three");
+        list.addLast("four");
+        list.addLast("five");
+
+        emptyList = new DummyLinkedList<>();
+
+        listWithOneElem = new DummyLinkedList<>();
+        listWithOneElem.addLast("one");
+    }
+
+    @Test
+    public void getTest() {
+        assertEquals("five", getter.get(1, list));
+        assertEquals("four", getter.get(2, list));
+        assertEquals("three", getter.get(3, list));
+        assertEquals("two", getter.get(4, list));
+        assertEquals("one", getter.get(5, list));
+        assertEquals("one", getter.get(1, listWithOneElem));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getFromEmptyTest() {
+        getter.get(1, emptyList);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getExceedSizeTest() {
+        getter.get(2, listWithOneElem);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getIncorrectIndexTest() {
+        getter.get(0, listWithOneElem);
+    }
+
+    @Test
+    public void getRecursiveTest() {
+        assertEquals("five", getter.getRecursive(1, list));
+        assertEquals("four", getter.getRecursive(2, list));
+        assertEquals("three", getter.getRecursive(3, list));
+        assertEquals("two", getter.getRecursive(4, list));
+        assertEquals("one", getter.getRecursive(5, list));
+        assertEquals("one", getter.getRecursive(1, listWithOneElem));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getRecursiveFromEmptyTest() {
+        getter.getRecursive(1, emptyList);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getRecursiveExceedSizeTest() {
+        getter.getRecursive(2, listWithOneElem);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getRecursiveIncorrectIndexTest() {
+        getter.getRecursive(0, listWithOneElem);
+    }
+
+    @Test
+    public void getIterativeTest() {
+        assertEquals("five", getter.getIterative(1, list));
+        assertEquals("four", getter.getIterative(2, list));
+        assertEquals("three", getter.getIterative(3, list));
+        assertEquals("two", getter.getIterative(4, list));
+        assertEquals("one", getter.getIterative(5, list));
+        assertEquals("one", getter.getIterative(1, listWithOneElem));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getIterativeFromEmptyTest() {
+        getter.getIterative(1, emptyList);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getIterativeExceedSizeTest() {
+        getter.getIterative(2, listWithOneElem);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getIterativeIncorrectIndexTest() {
+        getter.getIterative(0, listWithOneElem);
+    }
+}
